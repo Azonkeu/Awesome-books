8423c85e95e471316da304b57d6ce499e465f1b5
Azonkeu <mazonkeu@gmail.com>
2021-11-29 16:35:45 +0100

    Add the remove and the display functions

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ const newBook = () => {
     window.location.reload();
   }
 };
+
+// eslint-disable-next-line no-unused-vars
+function removeBook(id) {
+  const data = localStorage.getItem('data');
+  const convertedBooks = JSON.parse(data);
+  const remainingBooks = convertedBooks.filter((book) => book.id !== id);
+  const removedBooks = JSON.stringify(remainingBooks);
+  localStorage.setItem('data', removedBooks);
+  window.location.reload();
+}
+
+const displayBookList = () => {
+  const data = localStorage.getItem('data');
+  const books = JSON.parse(data);
+  document.querySelector('#book-list').innerHTML = 'No book saved';
+  if (books.length === 0) {
+    document.querySelector('#book-list').innerHTML = 'No book saved';
+  } else {
+    document.querySelector('#book-list').innerHTML = '';
+    let booklist = '';
+    books.forEach((book) => {
+      booklist += `<div>
+      <p class="tit">${book.title}</p>
+      <p class="auth">${book.author}</p>
+      <button class="delete" data-book-id = "${book.id}" id = "remove-button" onclick="removeBook(${book.id})">Remove</button>
+      <hr>
+      </div>`;
+    });
+    document.querySelector('#book-list').innerHTML = booklist;
+  }
+};
+
+window.addEventListener('DOMContentLoaded', displayBookList);
+
+document.querySelector('#book-form').addEventListener('submit', newBook);
